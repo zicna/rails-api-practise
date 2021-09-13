f51ec0e137e81ac1aa0336379c418b1a4f5bf7f0
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-13 11:45:31 -0500

    add logic if we submit invalid author

--- frontend/index.js
+++ frontend/index.js
@@ -2,9 +2,9 @@
 
 const URL = "http://localhost:3000/authors";
 const ul = document.getElementById("list-authors");
-const form = document.getElementById("new-author-form")
-const firstName = document.getElementById("first_name")
-const lastName = document.getElementById("last_name")
+const form = document.getElementById("new-author-form");
+const firstName = document.getElementById("first_name");
+const lastName = document.getElementById("last_name");
 
 function myFn() {
   fetch(URL)
@@ -13,46 +13,59 @@ function myFn() {
 }
 
 function addToUl(data) {
-  // console.log(data)
+  console.log(data);
   for (d of data) {
-    renderItem(d)
+    renderItem(d);
   }
 }
 
-function renderItem(data){
-  const li = document.createElement('li')
-  li.innerHTML = `
+function renderItem(data) {
+  const li = document.createElement("li");
+  if (data["attributes"]) {
+    // debugger
+    li.innerHTML = `
     <div>
-      name: ${data["attributes"]["first_name"]} ${data  ["attributes"]["last_name"]}
+      name: ${data["attributes"]["first_name"]} ${data["attributes"]["last_name"]}
     </div>
-  `
-  ul.appendChild(li)
+    <button class="edit-btn">Edit</button>
+    <button class="delete-btn">Delete</button>
+  `;
+  // li.addEventListener();
+  ul.appendChild(li);
+  } else if (data["error"]){
+    // debugger
+    alert(data["error"])
+    form.reset()
+  }
+  
 }
 
-
 let handleSubmit = (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
   const itemInfo = {
     author: {
       first_name: firstName.value,
-      last_name: lastName.value
-    }
-  }
+      last_name: lastName.value,
+    },
+  };
   // debugger
   const configObject = {
     method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-        Accept: 'application/json'
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
     //this was where your error was, not using JSON.stringify
-    body: JSON.stringify(itemInfo)
-  }
-  fetch(URL, configObject).then(response => response.json()).then(data => {
-    renderItem(data)
-  })
-}
-form.addEventListener("submit", handleSubmit)
+    body: JSON.stringify(itemInfo),
+  };
+  fetch(URL, configObject)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItem(data)
+      // debugger;
+    });
+};
+form.addEventListener("submit", handleSubmit);
 
 myFn();
