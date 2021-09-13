84dedec29c765ef6f725ffb8dd15de14f770233b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-13 11:42:40 -0500

    add validations for Author model

--- backend/app/models/author.rb
+++ backend/app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
     has_many :books
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :first_name, uniqueness: {scope: :last_name}
+    
 end
